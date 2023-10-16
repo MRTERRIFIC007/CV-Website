2de5c3a53987610df320ccc960cba6841771913f
Om Roy <113281086+MRTERRIFIC007@users.noreply.github.com>
2023-10-16 14:24:39 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+           'sans': ['Poppins','-apple-system', 'BlinkMacSystemFont'],
+           'serif': ['Georgia', 'Cambria'],
+           'mono': ['SFMono-Regular', 'Menlo'],
+           'display': ['Oswald'],
+           'body': ['Poppins','Open Sans'],
+          },
+          
+    extend: {
+      colors: {
+        'the-secondary' : '#233446',
+        'the-green' : '#3ED27E',
+      },
+      textColor: {
+        'own-blue' : '#96BDD8',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
